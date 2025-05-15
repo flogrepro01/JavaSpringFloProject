2152fa4aa09ac9dc1d475e19d6feb677f7bac251
g33ko <flogrenier.pro@gmail.com>
2025-05-15 15:34:50 +0200

    chore(Global): re setup project

---
+++ src/main/java/com/floworkspace/floproject/FloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.floworkspace.floproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloProjectApplication.class, args);
+	}
+
+}
